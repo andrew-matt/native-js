1cb19004630c10da29632b029b7285c56a45516a
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-02-11 13:45:15 +0300

    Create 02.ts

---
+++ 02/02.ts
@@ -0,0 +1,58 @@
+import React from "react";
+
+type TecnologiesType = {
+    id: number
+    title: string
+}
+
+type LocalCityType = {
+    title: string
+    country: string
+}
+
+type AddressType = {
+    street: string
+    city: LocalCityType
+}
+
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TecnologiesType>
+}
+
+export const student: StudentType = {
+    id: 1,
+    name: "Andrew",
+    age: 28,
+    isActive: true,
+    address: {
+        street: "Surganova 2",
+        city: {
+            title: "Minsk",
+            country: "Belarus"
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 2,
+            title: "JS"
+        },
+        {
+            id: 3,
+            title: "React"
+        }
+    ]
+}
+
+console.log(student.name)
+console.log(student.age)
+console.log(student.address.city.title)
+console.log(student.technologies[2].title)
