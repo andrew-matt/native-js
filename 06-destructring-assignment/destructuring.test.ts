d47412c634eb14ac5420fb2c5dc2d49c90665f99
andrew-matt <97734646+andrew-matt@users.noreply.github.com>
2022-02-24 17:41:12 +0300

    Update destructuring.test.ts

--- 06-destructring-assignment/destructuring.test.ts
+++ 06-destructring-assignment/destructuring.test.ts
@@ -6,7 +6,7 @@ beforeEach(() => {
     props = {
         name: "Andrew",
         age: 28,
-        lessons: [{title: "1"}, {title: "2"}],
+        lessons: [{title: "1"}, {title: "2"}, {title: "3"}],
         address: {
             street: {
                 title: "Nezavisimosti street"
@@ -21,10 +21,13 @@ test("1", () => {
     const {title} = props.address.street;
 
     expect(age).toBe(28);
-    expect(lessons.length).toBe(2);
+    expect(lessons.length).toBe(3);
     expect(title).toBe("Nezavisimosti street")
 })
 
 test("2", () => {
+    const [, l2, ...restLessons] = props.lessons
 
+    expect(l2.title).toBe('2')
+    expect(restLessons.length).toBe(1)
 })
